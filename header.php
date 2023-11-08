7241d7733a07df321b2d3b925c9a57f38c2369d2
Craig Bauer <craigbauer23@gmail.com>
2023-11-07 19:07:46 -0500

    added ga4 and google tag manager tags

--- header.php
+++ header.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html lang="en">
 <html>
 	<head>
+		<!-- Google Tag Manager -->
+		<script>
+			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-N9347G4K');
+		</script>
+		<!-- End Google Tag Manager -->
 		<!--[if IE]>
 		  <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
@@ -11,8 +20,24 @@
 		<!-- Mobile Device Support -->
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"> -->
+
+		<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZCN53WKMS0"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'G-ZCN53WKMS0');
+		</script>
 		
 		
 		<?php wp_head(); ?>
 	</head>
 	<body>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript>
+			<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9347G4K"
+			height="0" width="0" style="display:none;visibility:hidden"></iframe>
+		</noscript>
+		<!-- End Google Tag Manager (noscript) -->
